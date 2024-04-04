b35417c0052d46c04422e37442533164092e4be3
witalomelo <witalovmelo@gmail.com>
2024-04-04 16:06:57 -0300

    first commit

---
+++ ms-pagamento/src/main/java/br/com/food/pagamentos/repository/PagamentoRepository.java
@@ -0,0 +1,8 @@
+package br.com.food.pagamentos.repository;
+
+
+import br.com.food.pagamentos.model.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+}
---
+++ ms-pagamento/src/main/java/br/com/food/pagamentos/service/PagamentoService.java
@@ -0,0 +1,71 @@
+package br.com.food.pagamentos.service;
+
+import br.com.food.pagamentos.dto.PagamentoDto;
+import br.com.food.pagamentos.http.PedidoClient;
+import br.com.food.pagamentos.model.Pagamento;
+import br.com.food.pagamentos.model.Status;
+import br.com.food.pagamentos.repository.PagamentoRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PagamentoService {
+
+    @Autowired
+    private PagamentoRepository repository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private PedidoClient pedido;
+
+    public Page<PagamentoDto> obterTodos(Pageable paginacao) {
+        return repository
+                .findAll(paginacao)
+                .map(p -> modelMapper.map(p, PagamentoDto.class));
+    }
+
+    public PagamentoDto obterPorId(Long id) {
+        Pagamento pagamento = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException());
+
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public PagamentoDto criarPagamento(PagamentoDto dto) {
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setStatus(Status.CRIADO);
+        repository.save(pagamento);
+
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public PagamentoDto atualizarPagamento(Long id, PagamentoDto dto) {
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setId(id);
+        pagamento = repository.save(pagamento);
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public void excluirPagamento(Long id) {
+        repository.deleteById(id);
+    }
+
+    public void confirmarPagamento(Long id) {
+        Optional<Pagamento> pagamento = repository.findById(id);
+
+        if (!pagamento.isPresent()) {
+            throw new EntityNotFoundException();
+        }
+
+        pagamento.get().setStatus(Status.CONFIRMADO);
+        repository.save(pagamento.get());
+        pedido.atualizaPagamento(pagamento.get().getPedidoId());
+    }
+}
